b0c8cf800faf910e3daa68af047e442cbba81b2c
tyhranac <61034897+tyhranac@users.noreply.github.com>
2020-11-07 19:39:57 -0800

    switched input from xml to json

--- classificationDistances.py
+++ classificationDistances.py
@@ -8,41 +8,44 @@ Radius and equation from GIS Fundamentals 5th Ed. by Paul Bolstad
 """
 
 def gc_distance(point1, point2):
-	"""returns distance between point1 (lat, lon) and point2 (lat, lon) using great circle equation"""
-	lat1, lon1 = point1[0], point1[1]
-	lat2, lon2 = point2[0], point2[1]
+	"""returns distance between point1 (lat, lon) and point2 (lat, lon) in km using great circle equation"""
+	lon1, lat1 = point1[0], point1[1]
+	lon2, lat2 = point2[0], point2[1]
 	lat1, lat2, lon1, lon2 = map(math.radians, [lat1, lat2, lon1, lon2])
 	radius = 6378 # WGS84 equatorial radius in km
 
-	distance = radius * math.acos(math.sin(lat1) * math.sin(lat2) \
-		+ math.cos(lat1) * math.cos(lat2) * math.cos(lon1 - lon2))
+	trig = math.sin(lat1) * math.sin(lat2) + math.cos(lat1)\
+				 * math.cos(lat2) * math.cos(lon1 - lon2)
+
+	if trig >= 1:
+		distance = 0
+	else:
+		distance = radius * math.acos(trig)
 
 	return distance
 
 
-"""
-XML DOM parsing code from https://www.tutorialspoint.com/python/python_xml_processing.htm
-"""
-
-def class_distance(xml_file_path):
-	"""returns total distance between nodes of a given classification"""
-	DOMTree = parse(xml_file_path)
-	elements = DOMTree.documentElement
-	nodes = elements.getElementsByTagName("node")
+def class_distance(json_file_path):
+	"""returns total distance of ways for a given classification"""
+	ways = json.load(open(json_file_path))
 
 	total_distance = 0
 	coords = []
 
-	for node in nodes:
-		if node.hasAttribute("lat") and node.hasAttribute("lon"):
-			coords.insert(0, (float(node.getAttribute("lat")), float(node.getAttribute("lon"))))
-		if len(coords) < 2:
-			continue
-		else:
-			p1 = coords[0]
-			p2 = coords[1]
-			total_distance += gc_distance(p1, p2)
-			coords.pop()
+	for feature in ways["features"]:
+		if feature["geometry"]["type"] == "LineString":
+			way_distance = 0
+			for coord in feature["geometry"]["coordinates"]:
+				coords.insert(0, coord)
+				if len(coords) < 2:
+					continue
+				else:
+					p1 = coords[0]
+					p2 = coords[1]
+					way_distance += gc_distance(p1, p2)
+					coords.pop()
+
+			total_distance += way_distance
 
 	return total_distance
 
